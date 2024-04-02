bc550153c89a4170197950cefeb35daf4f8a8a30
ReeStick <minecraft.silkin@yandex.ru>
2024-04-02 19:01:45 +0300

    updated ngrok file and added requests log for admin

--- run_ngrok.py
+++ run_ngrok.py
@@ -1,7 +1,8 @@
 from pyngrok import ngrok
 
 ngrok.kill()
-auth_token = '2eKiVWV4iCYpqt1kM1Ln8R79t8N_3VptBx7rCdpgsZg6sihWA' # https://dashboard.ngrok.com/get-started/your-authtoken
-ngrok.set_auth_token(auth_token)
 
-ngrok.connect(7000)
+# ngrok config add-authtoken <token> 
+# https://dashboard.ngrok.com/get-started/your-authtoken
+
+print(ngrok.connect(7000))
