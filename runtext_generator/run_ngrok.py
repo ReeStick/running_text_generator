19455d837d1eff480bba31283aaa01101e621ff1
ReeStick <minecraft.silkin@yandex.ru>
2024-04-05 14:11:22 +0300

    fix

--- runtext_generator/run_ngrok.py
+++ runtext_generator/run_ngrok.py
@@ -2,7 +2,7 @@ from pyngrok import ngrok
 
 ngrok.kill()
 # получаем токен https://dashboard.ngrok.com/get-started/your-authtoken
-auth_token = "2eYFitCm5eCjF2yuZXvhLq3PX5B_4uUfZ7CaLs78pARJ3kkUh"
+auth_token = ""
 ngrok.set_auth_token(auth_token)
 ngstart = ngrok.connect(7000)
 print(ngstart)
